c344fa2ee47d2353025110eba71c409e8810f968
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-01-16 21:30:09 +0100

    Problem 4: Given a roman numeral, convert

--- Solution.cpp
+++ Solution.cpp
@@ -62,6 +62,54 @@ class Solution{
 
 
 
+//Problem 4: Given a roman numeral, convert
+  public:
+    int romanToInt(string s) {
+        int number;
+        int sum=0;
+        for(int i=s.length()-1;i>=0;i--) {
+            switch(s[i]) {
+                case 'I' :
+                    number=1;
+                    break;
+                case 'V' :
+                    number=5;
+                    break;
+                case 'X' :
+                    number=10;
+                    break;
+                case 'L' :
+                    number=50;
+                    break;
+                case 'C' :
+                    number=100;
+                    break;
+                case 'D' :
+                    number=500;
+                    break;
+                case 'M' :
+                    number=1000;
+                    break;
+            }
+            if((s[i]== 'X' || s[i]=='V') && i>0 && s[i-1]=='I')
+            {
+                number-=1;
+                i--;
+            }
+            else if((s[i]== 'C' || s[i]=='L') && i>0 && s[i-1]=='X')
+            {
+                number-=10;
+                i--;
+            }
+            else if((s[i]== 'D' || s[i]=='M') && i>0 && s[i-1]=='C')
+            {
+                number-=100;
+                i--;
+            }
+            sum+=number;
+        }
+        return sum;
+    }
 
 
 
