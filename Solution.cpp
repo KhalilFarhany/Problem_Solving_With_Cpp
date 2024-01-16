cd1ad6422fc0aa8ac83d75fd135a29a47495cb6f
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-01-16 21:29:11 +0100

    Problem 2: Merge the two lists into one sorted list

--- Solution.cpp
+++ Solution.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Definition for singly-linked list.
+struct ListNode {
+      int val;
+      ListNode *next;
+ };
+
 class Solution{
   public:
   //problem 1: Find duplicates in an array
@@ -18,5 +24,37 @@ class Solution{
 
         return result;
     }
+
+//Problem 2: Merge the two lists into one sorted list
+  public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+        ListNode* tmp1=list1;
+        ListNode* tmp2=list1;
+        while(tmp1->next != NULL) {
+            tmp1=tmp1->next;
+        }
+        tmp1->next=list2;
+
+        for(tmp1=list1 ; tmp1!=NULL ; tmp1=tmp1->next) {
+            for(tmp2=tmp1->next ; tmp2!=NULL ; tmp2=tmp2->next) {
+                if(tmp2->val < tmp1->val) {
+                    int num=tmp1->val;
+                    tmp1->val=tmp2->val;
+                    tmp2->val=num;
+                }
+            }
+        }
+        return list1;
+    }
+
+
+
+
+
+
+
+
 };
 
