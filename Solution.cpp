3aad07085eb25de31fcbafa7e2ae1ea1da1354b3
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-01-18 18:51:54 +0100

    Problem 8: Find the median of the two sorted arrays

--- Solution.cpp
+++ Solution.cpp
@@ -184,5 +184,21 @@ public:
         return v[0];
 }
 
+
+//Problem 8: Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+public:
+    double findMedianSortedArrays(vector<int>& nums1,vector<int>& nums2) {
+        vector <int> v=nums1;
+        for(int i=0;i<nums2.size();i++) {
+            v.push_back(nums2[i]);
+        }
+        sort(v.begin(),v.end());
+        if(v.size()%2==0)
+        {
+            return((double)(v[v.size()/2]+v[v.size()/2 - 1])/2);
+        }
+        else return v[v.size()/2];
+};
+
 };
 
