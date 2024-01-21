065e4dc61fee2a95574cabac03018d9c63c4ad13
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-01-21 21:18:10 +0100

    Problem 11: Remove Nth Node From End of List

--- Solution.cpp
+++ Solution.cpp
@@ -9,7 +9,7 @@ struct ListNode {
 
 class Solution{
   public:
-  //problem 1: Find duplicates in an array
+//problem 1: Find duplicates in an array
     vector<int> duplicates(long long arr[], int n) {
         sort(arr,arr+n);
         vector<int> result;
@@ -26,7 +26,6 @@ class Solution{
     }
 
 //Problem 2: Merge the two lists into one sorted list
-  public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1 == NULL) return list2;
         if(list2 == NULL) return list1;
@@ -51,7 +50,6 @@ class Solution{
 
 
 //Problem 3: find missing number in array
-  public:
     int missingNumber(vector<int>& array, int n) {
         int sum=(n * (n+1))/2;
         for(int i=0;i<array.size();i++) {
@@ -63,7 +61,6 @@ class Solution{
 
 
 //Problem 4: Given a roman numeral, convert
-  public:
     int romanToInt(string s) {
         int number;
         int sum=0;
@@ -113,7 +110,6 @@ class Solution{
 
 
 //Problem 5: Given an integer target,return true if target is in matrix
-public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         for(int i=0;i<matrix.size();i++) {
            for(int j=0;j<matrix[i].size();j++) {
@@ -124,7 +120,6 @@ public:
     }
 
 //Problem 6: remove the duplicates such that each unique element appears only once and return sorted
-public:
     void removeDuplicates(vector<int>&nums) {
         sort(nums.begin(), nums.end());
         for (int i = 1; i < nums.size(); i++) {
@@ -140,7 +135,6 @@ public:
 /*Problem 7: Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
 k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
 You may not alter the values in the list's nodes, only nodes themselves may be changed.*/
-public:
     int count(ListNode* head) {
         int count=0;
         while(head != NULL) {
@@ -186,7 +180,6 @@ public:
 
 
 //Problem 8: Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
-public:
     double findMedianSortedArrays(vector<int>& nums1,vector<int>& nums2) {
         vector <int> v=nums1;
         for(int i=0;i<nums2.size();i++) {
@@ -201,7 +194,6 @@ public:
 };
 
 //Problem 9: reverse Integer
-public:
     int reverse(int x) {
             long res=0;
             while(x != 0) {
@@ -215,7 +207,6 @@ public:
         }
 
 //Problem 10: Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
-public:
     int isValid(string str) {
             stack <char> st ;
             int i=0;
@@ -237,6 +228,34 @@ public:
             return false;
     }
 
+//Problem 11: Remove Nth Node From End of List
+    int countNode(ListNode* head) {
+        int count=0;
+        while(head != NULL ) {
+            count++;
+            head=head->next;
+        }
+        return count;
+    } 
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        n=countNode(head)-n;
+        int i=0;
+        while(i<n && curr!=NULL) {
+            prev=curr;
+            curr=curr->next;
+            i++;
+        }
+        if(curr!=NULL) {
+            if(prev==NULL) 
+                head=head->next;
+            else 
+                prev->next=curr->next;  
+            delete curr;
+        }
+        return head;
+    } 
 
 };
 
