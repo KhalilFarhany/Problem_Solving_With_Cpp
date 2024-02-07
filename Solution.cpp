07330b9f591764a3d946df3dc4e9383b2906c314
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-02-07 10:40:40 +0100

    Problem 30: Leetcode 88. Merge Sorted Array

--- Solution.cpp
+++ Solution.cpp
@@ -633,7 +633,7 @@ vector<int> sortBySelection(vector<int>& nums) {
         return nums;
     }
 
-//insertion sort
+//Problem 28: insertion sort
 vector<int> insertionSort(vector<int>& nums) {
 	for (int i = 1; i < nums.size(); i++) {
 		int nb = nums[i];
@@ -647,7 +647,7 @@ vector<int> insertionSort(vector<int>& nums) {
     return nums;
 }
 
-//bubble sorting
+//Problem 29: bubble sorting
 vector<int> bubbleSorting(vector<int>& nums) {
 	for (int i = 0; i < nums.size(); i++) {
 		for (int j = 0; j < nums.size()-1-i; j++) {
@@ -660,5 +660,19 @@ vector<int> bubbleSorting(vector<int>& nums) {
 	}
     return nums;
 }
+
+//problem 30: You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+//Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+//The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        while(m!=nums1.size()) {
+            nums1.pop_back();
+        }
+        for(int i=0;i<n;i++) {
+            nums1.push_back(nums2[i]);
+        }
+        sort(nums1.begin(),nums1.end());
+    }
+    
 };
 
