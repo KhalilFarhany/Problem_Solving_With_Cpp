e099cdcf0b01aa002b0a2b2187874e978fca84b1
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-02-08 13:26:02 +0100

    Problem 32(method2): Leetcode 383. Ransom Note

--- Solution.cpp
+++ Solution.cpp
@@ -705,5 +705,17 @@ bool canConstruct(string ransomNote, string magazine) {
         }
         return true;
     }
+//Problem 32(method 2): Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+//Each letter in magazine can only be used once in ransomNote.
+bool canConstruct2(string ransomNote, string magazine) {
+        for(int i=0;i<ransomNote.length();i++) {
+            int index=magazine.find(ransomNote[i]);
+            if(index == string::npos)
+                return false;
+            else 
+                magazine.erase(magazine.begin()+index,magazine.begin()+index+1);
+        }
+        return true;     
+    }
 };
 
