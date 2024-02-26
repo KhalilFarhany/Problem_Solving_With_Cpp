8bf57c3039f8c3f7d1687233388617eda01f3ef7
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-02-26 22:00:24 +0100

    Problem 42 : Leetcode 169. Majority Element

--- Solution.cpp
+++ Solution.cpp
@@ -922,4 +922,18 @@ int lengthOfLongestSubstring(string s) {
         }
         return mx;
     }
+//Problem 42 : Given an array nums of size n, return the majority element.
+//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++) {
+            mp[nums[i]]++;
+        }
+        for(unordered_map<int,int>::iterator it = mp.begin();it!=mp.end();it++) {
+            if(it->second > nums.size()/2) {
+                return it->first;
+            }
+        }
+        return 0;
+    }
 };
