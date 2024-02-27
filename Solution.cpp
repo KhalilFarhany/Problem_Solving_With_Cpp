531a67bab1a98ad76fef8940f32ffec45725c0c1
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-02-27 09:16:56 +0100

    Problem 46 : LeetCode 75. Sort Colors

--- Solution.cpp
+++ Solution.cpp
@@ -975,4 +975,31 @@ int missingNumber(vector<int>& nums) {
 
         return mid - sum; 
     }
+
+//Problem 45 : Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+//We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+//You must solve this problem without using the library's sort function.
+void sortColors(vector<int>& nums) {
+        int now=0;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i] == 0) {
+                swap(nums[now],nums[i]);
+                now++;
+            }
+        }
+
+        for(int i=now;i<nums.size();i++) {
+            if(nums[i] == 1) {
+                swap(nums[now],nums[i]);
+                now++;
+            }
+        }
+
+        for(int i=now;i<nums.size();i++) {
+            if(nums[i] == 2) {
+                swap(nums[now],nums[i]);
+                now++;
+            }
+        }
+    }
 };
