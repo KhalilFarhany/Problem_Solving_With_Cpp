f649dd9426fcf2d5f2dfacf6883deb5b71599311
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-03-04 21:39:30 +0100

    Problem 53 : LeetCode 948. Bag of Tokens

--- Solution.cpp
+++ Solution.cpp
@@ -1132,6 +1132,31 @@ ListNode* reverseListUsingRecursion(ListNode* head) {
         }
         return res;
     }
+
+//problem 53 : You start with an initial power of power, an initial score of 0, and a bag of tokens given as an integer array tokens, where each tokens[i] donates the value of tokeni.
+//Your goal is to maximize the total score by strategically playing these tokens. In one move, you can play an unplayed token in one of the two ways (but not both for the same token):
+//Face-up: If your current power is at least tokens[i], you may play tokeni, losing tokens[i] power and gaining 1 score.
+//Face-down: If your current score is at least 1, you may play tokeni, gaining tokens[i] power and losing 1 score.
+//Return the maximum possible score you can achieve after playing any number of tokens.
+int bagOfTokensScore(vector<int>& tokens, int power) {
+        
+        sort(tokens.begin(),tokens.end());
+        int maxScore = 0;
+        int score = 0;
+        int l=0;
+        int r=tokens.size() - 1;
+        while( l<=r) {
+            if(power >=tokens[l]) {
+                power -=tokens[l++];
+                score++;
+                maxScore = max(maxScore,score);
+            } else if (score>0) {
+                power +=tokens[r--];
+                score--;
+            } else break;
+        }
+        return maxScore;
+    }
 };
 
 
