10fe0096acd6b1f9ef0239063812bd6887442553
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-03-05 20:24:33 +0100

    Problem 56 : LeetCode 231. Power of Two

--- Solution.cpp
+++ Solution.cpp
@@ -1223,6 +1223,15 @@ vector<string> findRelativeRanks(vector<int>& score) {
     return res;
     }
 
+//problem 56 : Given an integer n, return true if it is a power of two. Otherwise, return false.
+//An integer n is a power of two, if there exists an integer x such that n == 2x.
+bool isPowerOfTwo(int n) {
+        if(n==0) return false;
+        if (n==1) return true; 
+        if(n%2 == 0) return isPowerOfTwo(n/2);
+        return false;
+}
+
 };
 
 
