60906ffe4bba93337f0feb69d91f2455878306d1
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-03-05 10:04:09 +0100

    Problem 55 : LeetCode 342. Power of Four

--- Solution.cpp
+++ Solution.cpp
@@ -1184,6 +1184,13 @@ int minimumLength(string s) {
         }
         return s.size();
     }
+//Given an integer n, return true if it is a power of four. Otherwise, return false.
+//An integer n is a power of four, if there exists an integer x such that n == 4x.
+ bool isPowerOfFour(int n) {
+        if(n==1) return true;
+        if(n<1 || n%4!=0) return false;
+        return isPowerOfFour(n/4); 
+    }
 
 };
 
