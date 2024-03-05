84248cb20ef47af4cf7d9abd30224cace189ca59
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-03-05 21:34:08 +0100

    Problem 57 : LeetCode 228. Summary Ranges

--- Solution.cpp
+++ Solution.cpp
@@ -1232,6 +1232,31 @@ bool isPowerOfTwo(int n) {
         return false;
 }
 
+
+//problem 57 : You are given a sorted unique integer array nums.
+//A range [a,b] is the set of all integers from a to b (inclusive).
+//Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+//Each range [a,b] in the list should be output as:
+//"a->b" if a != b
+//"a" if a == b
+vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> res;
+        for(int i=0;i<nums.size();i++)
+        {
+            string s=to_string(nums[i]);
+            bool entry=false;
+            while(i< nums.size()-1 && nums[i]+1 == nums[i+1]) {
+                i++;
+                entry=true;
+            }
+            if(entry) {
+                s+="->"+to_string(nums[i]);
+            }
+
+            res.push_back(s);
+        }
+        return res;
+    }
 };
 
 
