3030920e61a4dd308ae3bacd021c8fb4423587e9
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-03-11 15:27:33 +0100

    problem 65 : LeetCode 791. Custom Sort String

--- Solution.cpp
+++ Solution.cpp
@@ -1394,6 +1394,28 @@ string NumberToText(int Number) {
 		return   NumberToText(Number / 1000000000) + "Billions " + NumberToText(Number % 1000000000);
 	}
 }
+
+//Problem 63 : You are given two strings order and s. All the characters of order are unique and were sorted in some custom order previously.
+//Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
+//Return any permutation of s that satisfies this property.
+string customSortString(string order, string s) {
+        map<char,int>mp;
+        for(char i:s) {
+            mp[i]++;
+        }
+        string res="";
+        for(char i:order) {
+                for(int j=0;j<mp[i];j++) {
+                    res+=i;
+                }
+        }
+        for(char i:s) {
+            if(order.find(i) == string::npos)
+                res+=i;
+        }
+        return res;
+    }
+    
 };
 
 
