c2c6cc864c83b9258dc94a921e7f19cdaac4981a
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-03-16 09:55:07 +0100

    problem 69 : LeetCode 238. Product of Array Except Self

--- Solution.cpp
+++ Solution.cpp
@@ -1464,6 +1464,25 @@ int pivotInteger(int n) {
         return -1;
 }
 
+//Problem 66 : Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+//The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+//You must write an algorithm that runs in O(n) time and without using the division operation.
+vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> answer(nums.size(), 1);
+        int prefix = 1;
+        for(int i=0; i<nums.size(); i++) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+        int postfix = 1;
+        for(int i=nums.size()-1; i>=0; i--) { // iterate in reverse
+            answer[i] *= postfix; // dont replace the old prefix value
+            postfix *= nums[i];
+        }
+        return answer;
+    }
+
+
 };
 
 
