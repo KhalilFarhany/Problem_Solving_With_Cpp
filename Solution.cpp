6c1e471df2009096d240c80cfcba23cd9e766cfd
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-03-17 22:56:02 +0100

    problem 70 : LeetCode 57. Insert Interval

--- Solution.cpp
+++ Solution.cpp
@@ -1482,7 +1482,29 @@ vector<int> productExceptSelf(vector<int>& nums) {
         return answer;
     }
 
+//Problem 67 : You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+//Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+//Return intervals after the insertion.
+//Note that you don't need to modify intervals in-place. You can make a new array and return it
+vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+    vector<vector<int>> res;
+    int i = 0;
+    while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
+        res.push_back(intervals[i++]);
+    }
+    while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = min(newInterval[0], intervals[i][0]);
+        newInterval[1] = max(newInterval[1], intervals[i][1]);
+        ++i;
+    }
+    res.push_back(newInterval);
+    
+    while (i < intervals.size()) {
+        res.push_back(intervals[i++]);
+    }
 
+    return res;
+}
 };
 
 
