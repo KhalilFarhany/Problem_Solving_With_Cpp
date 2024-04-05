50da67f6ba066a20d21856cec58a77deddb97497
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-04-05 02:36:33 +0100

    problem 83 : LeetCode 1544. Make The String Great

--- Solution.cpp
+++ Solution.cpp
@@ -1788,6 +1788,26 @@ int maxDepth(string s) {
         }
         return mx;
     }
+
+//problem 83 : Given a string s of lower and upper case English letters.
+//A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where: 0 <= i <= s.length - 2
+//s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+//To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+//Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+//Notice that an empty string is also good.
+string makeGood(string s) {
+        int i = 0;
+        while (!s.empty() && i < s.size() - 1) {
+            if (int(s[i]) == int(s[i + 1]) + 32 || int(s[i]) == int(s[i + 1]) - 32) {
+                s = s.substr(0, i) + s.substr(i + 2);
+                if (i > 0)i--;
+            }
+            else
+                i++;
+        }
+        return s;
+}
+
 };
 
 
