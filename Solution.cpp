c86c9b87e7f6b90e90eae5927497dc491c38c7d7
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-04-16 06:27:15 +0100

    Problem 90 : leetcode 771. Jewels and Stones

--- Solution.cpp
+++ Solution.cpp
@@ -1944,6 +1944,22 @@ int sumNumbers(TreeNode* root) {
         sumNum(root,s,sum);
         return sum;
     }
+
+//problem 91 : You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+//Letters are case sensitive, so "a" is considered a different type of stone from "A".
+int numJewelsInStones(string jewels, string stones) {
+       unordered_map<char,bool>mpj;
+       for(char c : jewels) {
+            mpj[c]=true;
+       } 
+       int count=0;
+       for(char c : stones) {
+            if(mpj[c]) {
+                count++;
+            }
+       } 
+       return count;
+    }
 };
 
 
