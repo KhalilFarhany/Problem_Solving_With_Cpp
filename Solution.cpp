fe0df410bd07316c0144bbdbb55814a2c55be509
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-04-18 07:24:58 +0100

    Problem 91 : leetcode 463. Island Perimeter

--- Solution.cpp
+++ Solution.cpp
@@ -1960,6 +1960,39 @@ int numJewelsInStones(string jewels, string stones) {
        } 
        return count;
     }
+
+//Problem 91: You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+//Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+//The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+int islandPerimeter(vector<vector<int>>& grid) {
+        int lines=grid.size(),cols=grid[0].size();
+        int ans=0;
+        for(int i=0;i<lines;i++) {
+            for(int j=0;j<cols;j++) {
+                int sum=4;
+                if(grid[i][j]==1) {
+                    if(i>0 && grid[i-1][j]==1) {
+                        sum--;
+                    }
+                    if(j>0 && grid[i][j-1]==1) {
+                        sum--;
+                    }
+
+                    if(i<lines-1 && grid[i+1][j]==1) {
+                        sum--;
+                    }
+                    if(j<cols-1 && grid[i][j+1]==1) {
+                        sum--;
+                    }
+                    ans+=sum;
+                }
+            }
+         
+        }
+        return ans;
+    }
+
+    
 };
 
 
