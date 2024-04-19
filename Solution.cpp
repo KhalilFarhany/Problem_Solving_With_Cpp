573b440a058c7976472c749cb945bd15689ed211
khalyl <126724790+KhalilFarhany@users.noreply.github.com>
2024-04-19 18:17:40 +0100

    Problem 9 : leetcode 200. Number of Islands

--- Solution.cpp
+++ Solution.cpp
@@ -1992,7 +1992,32 @@ int islandPerimeter(vector<vector<int>>& grid) {
         return ans;
     }
 
-    
+//problem 9: Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), return the number of islands.
+//An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+    void tests(vector<vector<char>>& grid,int x,int y,int rows,int cols) {
+        if(x<0 || x>=rows || y<0 || y>=cols || grid[x][y] != '1')
+            return;
+        grid[x][y]='2';
+        tests(grid,x-1,y,rows,cols); 
+        tests(grid,x,y-1,rows,cols); 
+        tests(grid,x+1,y,rows,cols); 
+        tests(grid,x,y+1,rows,cols); 
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int rows=grid.size();
+        int cols=grid[0].size();
+        int ans=0;
+        for(int i=0;i<rows;i++) {
+            for(int j=0;j<cols;j++) {
+                if(grid[i][j]=='1') {
+                    ans++;
+                    tests(grid,i,j,rows,cols);
+                }
+            }
+        }
+        return ans;
+    }
+
 };
 
 
