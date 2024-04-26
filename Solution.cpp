72724d490717ce3b65909e5da05d08629273350f
khalilFarhani <khalilfarhany@gmail.com>
2024-04-26 16:41:30 +0200

    Problem 94 : LeetCode 104. Maximum Depth of Binary Tree

--- Solution.cpp
+++ Solution.cpp
@@ -2035,6 +2035,15 @@ int islandPerimeter(vector<vector<int>>& grid) {
         return ans;
     }
 
+//problem 93: Given the root of a binary tree, return its maximum depth.
+//A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+   int maxDepth(TreeNode* root) {
+       if (root == nullptr)
+           return 0;
+       return
+           1 + max(maxDepth(root->left), maxDepth(root->right));
+   }
+
 };
 
 
