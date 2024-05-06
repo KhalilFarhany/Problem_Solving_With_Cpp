c504e0510cdc98103b8dd7d9d7356b4320eeff72
khalilFarhani <khalilfarhany@gmail.com>
2024-05-06 18:54:24 +0100

    Problem 105 : LeetCode 413. Arithmetic Slices

--- Solution.cpp
+++ Solution.cpp
@@ -2272,6 +2272,26 @@ int islandPerimeter(vector<vector<int>>& grid) {
        return false;
    }
 
+//Problem 105 : An integer array is called arithmetic if it consists of at least three elements and if the difference between any two consecutive elements is the same.
+//For example, [1, 3, 5, 7, 9], [7, 7, 7, 7], and [3, -1, -5, -9] are arithmetic sequences. Given an integer array nums, return the number of arithmetic subarrays of nums. A subarray is a contiguous subsequence of the array.
+   int numberOfArithmeticSlices(vector<int>& nums) {
+       if (nums.size() < 3)
+           return 0;
+       int sum = 0;
+       int count = 0;
+       int diff = nums[1] - nums[0];
+       for (int i = 2; i < nums.size(); i++) {
+           if (nums[i] - nums[i - 1] == diff) {
+               count++;
+           }
+           else {
+               count = 0;
+               diff = nums[i] - nums[i - 1];
+           }
+           sum += count;
+       }
+       return sum;
+   }
 };
 
 
