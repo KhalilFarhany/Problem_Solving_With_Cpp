b66b9ab7bb17fd63b172048ba1e0bd05a3597af9
khalilFarhani <khalilfarhany@gmail.com>
2024-05-11 12:03:29 +0100

    Problem 111 : LeetCode 1323. Maximum 69 Number

--- Solution.cpp
+++ Solution.cpp
@@ -2421,6 +2421,23 @@ int islandPerimeter(vector<vector<int>>& grid) {
        }
        return ans;
    }
+
+   // problem 111 : You are given a positive integer num consisting only of digits 6 and 9.
+   // Return the maximum number you can get by changing at most one digit(6 becomes 9, and 9 becomes 6).
+   int maximum69Number(int num) {
+       string str = to_string(num);
+       int max = num;
+       for (int i = 0; i < str.size(); i++) {
+           if (str[i] == '6') {
+               str[i] = '9';
+               int val = stoi(str);
+               if (val > max)
+                   max = val;
+               str[i] = '6';
+           }
+       }
+       return max;
+   }
 };
 
 
