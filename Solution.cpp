3e918e10acc7f5b1000c34e448f4c2b8e846bfe3
khalilFarhani <khalilfarhany@gmail.com>
2024-05-12 21:43:35 +0100

    Problem 112 : LeetCode 2373. Largest Local Values in a Matrix

--- Solution.cpp
+++ Solution.cpp
@@ -2438,6 +2438,25 @@ int islandPerimeter(vector<vector<int>>& grid) {
        }
        return max;
    }
+
+   // problem 112 : You are given an n x n integer matrix grid. Generate an integer matrix maxLocal of size(n - 2) x(n - 2) such that : maxLocal[i][j] is equal to the largest value of the 3 x 3 matrix in grid centered around row i + 1 and column j + 1. In other words, we want to find the largest value in every contiguous 3 x 3 matrix in grid.
+   // Return the generated matrix.
+   vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+       int n = grid.size();
+       vector<vector<int>> ans(n - 2, vector<int>(n - 2, 0));
+       for (int i = 0; i < n - 2; i++) {
+           for (int j = 0; j < n - 2; j++) {
+               int mx = grid[i][j];
+               for (int s = i; s < i + 3; s++) {
+                   for (int k = j; k < j + 3; k++) {
+                       mx = max(grid[s][k], mx);
+                   }
+               }
+               ans[i][j] = mx;
+           }
+       }
+       return ans;
+   }
 };
 
 
