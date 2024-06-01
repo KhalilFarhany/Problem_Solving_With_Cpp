318ab895812efb11bcf490864d94f03f8ca81962
khalilFarhani <khalilfarhany@gmail.com>
2024-06-01 08:35:45 +0100

    Problem 127 : LeetCode 3110. Score of a String

--- Solution.cpp
+++ Solution.cpp
@@ -2804,6 +2804,17 @@ int islandPerimeter(vector<vector<int>>& grid) {
    }
 
 
+   // problem 127 : You are given a string s.The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+   // Return the score of s.
+   int scoreOfString(string s) {
+       int sum = 0;
+       for (int i = 0; i < s.size() - 1; i++) {
+           sum += abs(s[i] - s[i + 1]);
+       }
+       return sum;
+   }
+
+
 };
 
 
