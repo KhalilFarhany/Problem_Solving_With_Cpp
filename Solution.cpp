4cdaea31c6ae905bb10fee2969edb7f9be28f027
khalilFarhani <khalilfarhany@gmail.com>
2024-06-02 10:06:39 +0100

    Problem 129 : LeetCode 2923. Find Champion I

--- Solution.cpp
+++ Solution.cpp
@@ -2821,6 +2821,29 @@ int islandPerimeter(vector<vector<int>>& grid) {
        }
    }
 
+   // problem 129 : There are n teams numbered from 0 to n - 1 in a tournament.
+   // Given a 0 - indexed 2D boolean matrix grid of size n * n.For all i, j that 0 <= i, j <= n - 1 and i != j team i is stronger than team j if grid[i][j] == 1, otherwise, team j is stronger than team i.
+   // Team a will be the champion of the tournament if there is no team b that is stronger than team a.
+   // Return the team that will be the champion of the tournament.
+   int findChampion1(vector<vector<int>>& grid) {
+       int n = grid.size();
+       int mx = 0;
+       int team = 0;
+       for (int i = 0; i < n; i++) {
+           int count = 0;
+           for (int j = 0; j < n; j++) {
+               if (i != j && grid[i][j] == 1) {
+                   count++;
+               }
+           }
+           if (count > mx) {
+               team = i;
+               mx = count;
+           }
+       }
+       return team;
+   }
+
 
 };
 
