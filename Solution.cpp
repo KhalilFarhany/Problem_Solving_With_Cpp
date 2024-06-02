2ca2114d7f77d3dbf6a3e3988c2b4fc16e3edc93
khalilFarhani <khalilfarhany@gmail.com>
2024-06-02 10:08:10 +0100

    Problem 130 : LeetCode 2924. Find Champion II

--- Solution.cpp
+++ Solution.cpp
@@ -2845,6 +2845,30 @@ int islandPerimeter(vector<vector<int>>& grid) {
    }
 
 
+   // problem 130 : There are n teams numbered from 0 to n - 1 in a tournament; each team is also a node in a DAG.
+   // You are given the integer n and a 0 - indexed 2D integer array edges of length m representing the DAG, where edges[i] = [ui, vi] indicates that there is a directed edge from team ui to team vi in the graph.
+   // A directed edge from a to b in the graph means that team a is stronger than team b and team b is weaker than team a.
+   // Team a will be the champion of the tournament if there is no team b that is stronger than team a.
+   // Return the team that will be the champion of the tournament if there is a unique champion, otherwise, return -1.
+   int findChampion2(int n, vector<vector<int>>& edges) {
+       vector<int>v(n);
+       for (int i = 0; i < n; i++) {
+           v[i] = i;
+       }
+       for (int i = 0; i < edges.size(); i++) {
+           if (v[edges[i][1]] = -1);
+       }
+       int ans = -1;
+       for (int i = 0; i < n; i++) {
+           if (v[i] != -1)
+               if (ans != -1)
+                   return -1;
+               else
+                   ans = v[i];
+       }
+       return ans;
+   }
+
 };
 
 
