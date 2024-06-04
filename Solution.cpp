febdef7d1495a77940e2531c0ac5178932f56128
khalilFarhani <khalilfarhany@gmail.com>
2024-06-04 17:17:22 +0100

    Problem 135 : LeetCode 1710. Maximum Units on a Truck

--- Solution.cpp
+++ Solution.cpp
@@ -2973,6 +2973,25 @@ int islandPerimeter(vector<vector<int>>& grid) {
        }
        return (ans + (hasOdd ? 1 : 0));
    }
+
+   // problem 135 : You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes, where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+   // numberOfBoxesi is the number of boxes of type i. numberOfUnitsPerBoxi is the number of units in each box of the type i. You are also given an integer truckSize, which is the maximum number of boxes that can be put on the truck.You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+   // Return the maximum total number of units that can be put on the truck.
+   int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+       sort(boxTypes.begin(), boxTypes.end(), [](const vector<int>& a, const vector<int>& b) {
+           return a[1] > b[1];
+           });
+       int ans = 0;
+       for (int i = 0; i < boxTypes.size(); i++) {
+           truckSize -= boxTypes[i][0];
+           if (truckSize < 0) {
+               ans += ((boxTypes[i][0] + truckSize) * boxTypes[i][1]);
+               break;
+           }
+           ans += boxTypes[i][0] * boxTypes[i][1];
+       }
+       return ans;
+   }
 };
 
 
